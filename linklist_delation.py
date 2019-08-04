5f04a0566a58eacaa61c54722e3ccc34ca9b2be3
shadiqurshaon <shadiqurshaon@gmail.com>
2019-08-04 13:33:24 +0600

    linklist insert and deletion

--- linklist_delation.py
+++ linklist_delation.py
@@ -12,6 +12,35 @@ class linklist:
         temp_node.next = self.head
         self.head = temp_node
 
+    def insert_at_end(self,data):
+        newNode = Node(data)
+        temp = self.head
+        while(temp.next!=None):
+            temp = temp.next
+        temp.next = newNode
+        
+    def insert_after(self,aftervalue,inserted_value):
+        newNode = Node(inserted_value)
+        temp = self.head
+        while(temp!=None):
+            if temp.data == aftervalue:
+                break            
+            temp = temp.next
+        newNode.next = temp.next
+        temp.next = newNode
+
+    def deleteNode(self,data):
+        temp = self.head
+        while(temp.next!=None):
+            if temp.next.data == data:
+                break
+            temp = temp.next
+        temp.next = temp.next.next
+
+
+
+
+
     def print_list(self):
         temp = self.head
         while(temp!=None):
@@ -21,7 +50,12 @@ if __name__ == "__main__":
     linklist = linklist()
     linklist.insert_in_begining(3)
     linklist.insert_in_begining(2)
+    linklist.insert_at_end(4)
     linklist.insert_in_begining(1)
+    linklist.insert_after(2,5)
+    linklist.print_list()
+    print('------')
+    linklist.deleteNode(5)
     linklist.print_list()
 
         
