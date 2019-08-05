0ea55a551bba0822a916aa22c7b22c4a0b01ebf2
shadiqurshaon <shadiqurshaon@gmail.com>
2019-08-05 18:50:27 +0600

    modification remove duplicate array

--- remove_duplicate_array.py
+++ remove_duplicate_array.py
@@ -1,9 +1,9 @@
-array = [1, 1, 2]
-newarray = {}
-for item in array:
-    if item not in newarray:
-        newarray[item] = item
-print(len(newarray))
+# array = [1, 1, 2]
+# newarray = {}
+# for item in array:
+#     if item not in newarray:
+#         newarray[item] = item
+# print(len(newarray))
 
 # nums = [1, 1, 2, 1, 2, 1, 3, 3]
 # unique_nums = []
@@ -15,3 +15,20 @@ print(len(newarray))
 #         unique_nums.append(nums[i])
 
 # print(len(unique_nums))
+
+
+class dd:
+        def compare2(self,list1,list2):
+                list1_dict = {i:i for i in list1}
+                
+                for item in list2:
+                        if item not in list1_dict:
+                                print('false')
+
+if __name__ == "__main__":
+        dd= dd()
+        list1 = [1, 2, 3, 4, 5]
+        list2 = [7, 3, 4, 5]
+        dd.compare2(list1,list2)
+
+    
