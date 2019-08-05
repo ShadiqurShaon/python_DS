7f01192dfd138ba4622b335efa4167f1e2d7e1de
shadiqurshaon <shadiqurshaon@gmail.com>
2019-08-05 13:17:40 +0600

    nth linklist node from last

---
+++ find_nthnode_fromlast_linklist.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class linklist:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self,data):
+        newnode = Node(data)
+        newnode.next = self.head
+        self.head = newnode
+
+    def print_list(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+    
+    def find_nth_from_last(self,position):
+        first = self.head
+        second = self.head
+        for i in range(1,position+1):
+            second = second.next
+        while(second):
+            first = first.next
+            second = second.next
+
+        print(first.data)
+
+
+if __name__ == "__main__":
+
+    link = linklist()
+    link.insert(5)
+    link.insert(4)
+    link.insert(3)
+    link.insert(2)
+    link.insert(1)
+    link.print_list()
+    print("-------")
+    link.find_nth_from_last(3)
