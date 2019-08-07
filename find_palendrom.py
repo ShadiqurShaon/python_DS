e978e604ff25a9181f7d3b8544bf22e0ac1550a7
shadiqurshaon <shadiqurshaon@gmail.com>
2019-08-07 11:55:50 +0600

    palendrom in linklist

---
+++ find_palendrom.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self,data):
+        self.data  = data
+        self.next = None
+
+class Link_list:
+    def __init__(self):
+        self.head = None
+
+    def insert(self,data):
+        tempNode = Node(data)
+        tempNode.next = self.head
+        self.head = tempNode
+    
+    def print_list(self):
+        current = self.head
+        while(current):
+            print(current.data)
+            current = current.next
+
+    def find_palendrom(self):
+        current  = self.head
+        stack = []
+        
+        while(current):
+            stack.append(current.data)
+            current = current.next
+
+        current2 = self.head
+        while(current2):
+            if current2.data != stack.pop():
+                return "not palendrom"
+            current2 = current2.next
+        return "palendrom"
+    
+if __name__ == "__main__":
+    llist = Link_list()
+    llist.insert(3)
+    llist.insert(2)
+    llist.insert(1)
+    llist.insert(2)
+    llist.insert(3)
+
+    llist.print_list()
+
+    print(llist.find_palendrom())
+
+    
+            
