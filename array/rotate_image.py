f4e05676db09820310fbdebac2c20d6c3b8b394d
shadiqurshaon <shadiqurshaon@gmail.com>
2019-12-13 23:57:51 +0600

    rotate image

--- array/rotate_image.py
+++ array/rotate_image.py
@@ -1,5 +1,9 @@
 def rotate(a):
   dic = {}
+  print(len(a))
+  for i in range(len(a)//2):
+    a[i],a[(len(a)-1)-i]=a[(len(a)-1)-i],a[i]
+
   for i in range(len(a)):
     for j in range(len(a)):
       if i!=j and str([i,j]) not in dic:
@@ -8,7 +12,9 @@ def rotate(a):
         dic[str([j,i])]=[j,i]
       # print(a[i][j])
   return a
-arr = [[1,2,3],
-      [1,2,3],
-      [1,2,3]]
+arr = [[ 5, 1, 9,11,1],
+  [ 2, 4, 8,10,2],
+  [13, 3, 6, 7,3],
+  [15,14,12,16,4],
+  [1,2,3,4,5]]
 print(rotate(arr))
