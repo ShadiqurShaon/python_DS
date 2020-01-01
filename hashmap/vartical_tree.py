60baf27e56d4853e4829a123597b6920c82ca22a
Shadiqur Rahaman <shadiqurshaon@gmail.com>
2020-01-01 16:54:48 +0600

    add subarray of a given sum

--- hashmap/vartical_tree.py
+++ hashmap/vartical_tree.py
@@ -1,34 +1,16 @@
-def create(n):
-    re = []
-    for i in range(n):
-        re.append([0]*n)
-    i=0
-    j=0
-    fr=0
-    lr=n-1
-    fc=0
-    lc=n-1
-    for item in range(n*n):
-        re[j][i]=item+1 
-        if i!=lc and j==fr:
-            i+=1
-        elif i==lc and j!=lr:
-            j+=1
-        elif i<=lc and j==lr and i!=fc:
-            i-=1
-        elif i==fc and j<=lr and j!=fr:
-            j-=1
-        if i==fc and j==lr:
-            fr+=1
-            lc-=1
-        elif i==fr and j==fc+1:
-            lr-=1
-            fc+=1
-        
+def generateMatrix(n):
+    
+    result = [[0 for i in range(n)] for j in range(n)]
+    coord = [[(i,j) for j in range(n)] for i in range(n)]
+    
+    count = 1
+    
+    while coord:
+        for x, y in coord.pop(0):
+            result[x][y] = count
+            count += 1
+        coord = zip *coord[::-1]
 
+    return result   
 
-        
-    for i in re:
-        print(i,end='\n')
-
-create(10)
+print(generateMatrix(5))
