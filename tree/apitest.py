1977baeaaa6219a7655c5b188f5d30f81183366c
shadiqurshaon <shadiqurshaon@gmail.com>
2020-01-08 21:05:28 +0600

    8-01-2020

--- tree/apitest.py
+++ tree/apitest.py
@@ -1,11 +1,9 @@
 def jump(arr,index):
-  if index>=len(arr):
+  if index>=len(arr)-1:
     return True
   elif arr[index]==0:
     return False
   else:
-    if index==0:
-      return jump(arr,arr[0])
-    else:
-      return jump(arr,index+arr[index])
-print(jump( [2,0,1],0))
+    for i in range(1,arr[index]):
+      return jump(arr,index+arr[index+i])
+print(jump( [3,1,1,1],0))
