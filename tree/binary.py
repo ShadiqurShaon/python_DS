6d3a952a2a84dcbe3c63fbe792b98243a7fe6cf1
Shadiqur Rahaman <shadiqurshaon@gmail.com>
2020-01-09 14:23:56 +0600

    implement avl tree

--- tree/binary.py
+++ tree/binary.py
@@ -13,7 +13,36 @@ class Bst:
             root.left = self.insert(root.left,data)
         else:
             root.right = self.insert(root.right,data)
+
+        root.height = 1+max(self.get_height(root.left),self.get_height(root.right))
+        balance = self.get_balance(root)
+
+        if balance>1 and data<root.left.data:
+            # left left
+            # right rotate
+            return self.right_rotate(root)
+        if balance>1 and data>root.left.data:
+            # left right
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        if balance<-1 and data>root.right.data:
+            # right right
+            return self.left_rotate(root)
+        if balance<-1 and data<root.right.data:
+            # right left
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
         return root
+    def get_height(self,root):
+        if not root:
+            return 0
+        return root.height
+
+    def get_balance(self,root):
+        if not root:
+            return 0
+        return self.get_height(root.left)-self.get_height(root.right)
 
     def print_tree(self,root):
         if not root:
@@ -22,28 +51,42 @@ class Bst:
         self.print_tree(root.left)
         self.print_tree(root.right)
 
-    def rotate(self,root):
+    def right_rotate(self,root):
         y = root.left
         temp = y.right
         y.right = root
         root.left = temp
 
+        root.height = 1+max(self.get_height(root.left),self.get_height(root.right))
+        y.height = 1+max(self.get_height(y.left),self.get_height(y.right))
+
         return y
+    
+    def left_rotate(self,root):
+        y = root.right
+        temp = y.left
+        y.left = root
+        root.right = temp
+        root.height = 1+max(self.get_height(root.left),self.get_height(root.right))
+        y.height =  1+max(self.get_height(y.left),self.get_height(y.right))
+        return y
+    
+    
 
 
 
-root = None
+
 bst = Bst()
-root = bst.insert(root,20)
+root = None
 root = bst.insert(root,10)
-root = bst.insert(root,40)
+root = bst.insert(root,20)
 root = bst.insert(root,30)
+root = bst.insert(root,40)
 root = bst.insert(root,50)
 root = bst.insert(root,25)
-root = bst.insert(root,35)
-newroot = root.right
-rotate_root = bst.rotate(newroot)
-bst.print_tree(newroot)
+# root = bst.insert(root,35)
+
+bst.print_tree(root)
         
 
 
