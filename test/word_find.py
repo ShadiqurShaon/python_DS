2971a4624b72598d8c6a0a5dc4de93aa94f107da
shadiqurshaon <shadiqurshaon@gmail.com>
2020-01-25 23:02:42 +0600

    find word in matrix

--- test/word_find.py
+++ test/word_find.py
@@ -1,11 +1,15 @@
-def find(word,strat_word,mat,col,row):
-    if len(word)-1==strat_word:
+def find(word,strat_word,mat,row,col):
+    if len(word)==strat_word:
         return True
     if (col<0 or row<0 or  row>=len(mat) or col>=len(mat[0])):
         return False
 
-    if word[strat_word]==mat[col][row]:
-        return find(word,strat_word+1,mat,col,row+1) or find(word,strat_word+1,mat,col,row-1) or find(word,strat_word+1,mat,col+1,row) or find(word,strat_word+1,mat,col-1,row)
+    if word[strat_word]==mat[row][col]:
+        temp = mat[row][col]
+        mat[row][col] = 0
+        fin = find(word,strat_word+1,mat,row+1,col) or find(word,strat_word+1,mat,row-1,col) or find(word,strat_word+1,mat,row,col+1) or find(word,strat_word+1,mat,row,col-1)
+        mat[row][col] = temp
+        return fin
     else:
         return False
         
@@ -15,14 +19,14 @@ def mainfunc():
     mat = [
     ['A','B','C','E'],
     ['S','F','C','S'],
-    ['A','E','E','E']
+    ['A','D','E','E']
     ]
-    word = "ABCCED"
+    word = "SEE"
 
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             if word[0]==mat[i][j]:
-                bol = find(word,0,mat,j,i)
+                bol = find(word,0,mat,i,j)
                 if bol==True:
                     return True
     return False
